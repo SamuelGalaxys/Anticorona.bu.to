05995b9024672bf22a499730bbf50ca167da4a5b
SamuelGalaxys <60698168+SamuelGalaxys@users.noreply.github.com>
2020-02-05 22:56:16 +0900

    Anticorona HTML
    Wuhan corona show servise

---
+++ assets/js/demo.js
@@ -0,0 +1,79 @@
+﻿
+Circles.create({
+	id:           'task-complete',
+	radius:       75,
+	value:        80,
+	maxValue:     100,
+	width:        8,
+	text:         function(value){return value + '%';},
+	colors:       ['#eee', '#1D62F0'],
+	duration:     400,
+	wrpClass:     'circles-wrp',
+	textClass:    'circles-text',
+	styleWrapper: true,
+	styleText:    true
+})
+
+
+ 
+// monthlyChart
+
+Chartist.Pie('#monthlyChart', {
+	labels: ['발열 47%', '호흡곤란 23%', '기침 30%'],
+	series: [47, 23, 30]
+}, {
+	plugins: [
+	Chartist.plugins.tooltip()
+	]
+});
+
+// trafficChart
+var chart = new Chartist.Line('#trafficChart', {
+	labels: [1, 2, 3, 4, 5, 6, 7],
+	series: [
+	[5, 9, 7, 8, 5, 3, 5],
+	[6, 9, 5, 10, 2, 3, 7],
+	[2, 7, 4, 10, 7, 6, 2]
+	]
+}, {
+	plugins: [
+	Chartist.plugins.tooltip()
+	],
+	low: 0,
+	height: "245px",
+});
+
+// salesChart
+var dataSales = {
+	labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+	series: [
+	[5, 4, 3, 7, 5, 10, 3, 4, 8, 10, 6, 8],
+	[3, 2, 9, 5, 4, 6, 4, 6, 7, 8, 7, 4]
+	]
+}
+
+var optionChartSales = {
+	plugins: [
+	Chartist.plugins.tooltip()
+	],
+	seriesBarDistance: 10,
+	axisX: {
+		showGrid: false
+	},
+	height: "245px",
+}
+
+var responsiveChartSales = [
+['screen and (max-width: 640px)', {
+	seriesBarDistance: 5,
+	axisX: {
+		labelInterpolationFnc: function (value) {
+			return value[0];
+		}
+	}
+}]
+];
+
+Chartist.Bar('#salesChart', dataSales, optionChartSales, responsiveChartSales);
+
+ 
